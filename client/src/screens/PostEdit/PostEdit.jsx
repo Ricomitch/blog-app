39070b3ad6fb37ea0a8dd4f97a16f7e57c5e353d
Adam Mosebach <adam_mosebach@Adams-MBP.home>
2020-08-18 19:16:51 -0400

    post-edit done

--- client/src/screens/PostEdit/PostEdit.jsx
+++ client/src/screens/PostEdit/PostEdit.jsx
@@ -1,52 +1,92 @@
-import React, { useState, useEffect } from 'react'
-import { useParams, Redirect } from 'react-router-dom'
-import { getPost, updatePost } from '../../services/posts'
+import React, { useState, useEffect } from 'react';
+import { useParams, Redirect } from 'react-router-dom';
+import { getPost, updatePost } from '../../services/posts';
 
 const PostEdit = (props) => {
   const [post, setPost] = useState({
     title: '',
     imgURL: '',
     content: '',
-    userId: ''
-  })
+    userId: '',
+  });
 
-  const [isUpdated, setUpdated] = useState(false)
-  const { id } = useParams()
-  
+  const [isUpdated, setUpdated] = useState(false);
+  const { id } = useParams();
+
   useEffect(() => {
     async function fetchPost() {
-      const post = await getPost(id)
-      setPost(post)
+      const post = await getPost(id);
+      setPost(post);
     }
-    fetchPost()
-  }, [id])
+    fetchPost();
+  }, [id]);
 
-  function handleChange (event) {
-    const { name, value } = event.target
+  function handleChange(event) {
+    const { name, value } = event.target;
     setPost({
       ...post,
-      [name]: value
-    })
+      [name]: value,
+    });
   }
 
   async function handleSubmit(event) {
-    event.preventDefault()
-    const { id } = props.match.params
-    const updated = await updatePost(id, post)
-    setUpdated(updated)
+    event.preventDefault();
+    const { id } = props.match.params;
+    const updated = await updatePost(id, post);
+    setUpdated(updated);
   }
 
-  // if (isUpdated) {
-  //   return <Redirect to={`/posts/${props.match.params.id}`} />
-  // }
+  if (isUpdated) {
+    return <Redirect to={`/posts/${props.match.params.id}`} />;
+  }
 
   return (
     <div>
-      <input value={post.title} />
+      <div className='post-edit'>
+        <div className='image-container'>
+          <img className='edit-post-image' src={post.imgURL} alt={post.title} />
+          <form onSubmit={handleSubmit}>
+            <input
+              className='edit-input-image-link'
+              placeholder='Image Link'
+              value={post.imgURL}
+              name='imgURL'
+              required
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+        <form className='edit-form' onSubmit={handleSubmit}>
+          <input
+            className='input-title'
+            placeholder='Title'
+            value={post.title}
+            name='title'
+            requiredonChange={handleChange}
+          />
+          <textarea
+            className='textarea-content'
+            rows={10}
+            cols={78}
+            placeholder='Content'
+            value={post.content}
+            name='content'
+            required
+            onChange={handleChange}
+          />
+          <input
+            className='input-userId'
+            placeholder='User ID'
+            value={post.userId}
+            name='userId'
+            required
+            onChange={handleChange}
+          />
+          <button type='submit' className='save-button'>Save</button>
+        </form>
+      </div>
     </div>
-  )
+  );
+};
 
-
-}
-
-export default PostEdit
+export default PostEdit;

246c346fb4ab1f2857a504f4f39f4a14fd44134b
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-19 16:43:11 -0400

    adjustments

--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -16,9 +16,7 @@ function Posts() {
   
   const capitalized = (input) => {
     let splitWords = input.split(' ')
-    // console.log(splitWords)
     const result = splitWords.map(word => `${word.charAt(0).toUpperCase()}${word.substring(1)}`)
-    console.log(result)
     return result.join(' ')
   }
   
@@ -27,7 +25,6 @@ function Posts() {
     return <Post _id={post._id} title={capitalized(post.title)} imgURL={post.imgURL} textSample={post.content} key={index} authorName={post.userId.username} />
   })
   
-  console.log(allPosts)
   return (
     <div className="posts-container">
       {/* <Search onSubmit={handleSubmit} onChange={handleSearch} />

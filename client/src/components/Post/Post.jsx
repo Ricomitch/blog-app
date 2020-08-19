a299ea2b9721b3a478ad43d5df5c2629c7fe805e
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-18 21:56:48 -0400

    Title capitalization

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -1,10 +1,8 @@
 import React from 'react'
-// import './Post.css'
+import './Post.css'
 import { Link } from 'react-router-dom'
 
 function Post(props) {
-  
-  
   
   return (
     <>
@@ -13,7 +11,7 @@ function Post(props) {
           className="post-image" />
         <div className="post-title">{props.title}</div>
         <div className="author">placeholder author name</div>
-        <div className="summary-text"></div>
+        <div className="summary-text">{props.textSample.substring(0, 100)}</div>
       </Link>
 
     </>
--- client/src/screens/Posts/Posts.jsx
+++ client/src/screens/Posts/Posts.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import Post from '../../components/Post/Post'
 import { getPosts } from '../../services/posts'
+import './Posts.css'
 
 function Posts() {
   const [allPosts, setAllPosts] = useState([])
@@ -8,25 +9,34 @@ function Posts() {
   useEffect(() => {
     const fetchPosts = async () => {
       const posts = await getPosts()
+      posts.forEach(post => {
+        post.title = capitalized(post.title)
+      })
       setAllPosts(posts)
     }
     fetchPosts()
   }, [])
   
+  const capitalized = (input) => {
+    let splitWords = input.split(' ')
+    console.log(splitWords)
+    const result = splitWords.map(word => `${word[0].toUpperCase()}${word.substring(1)}`)
+    
+    return result.join(' ')
+  }
+  
   const postsJSX = allPosts.map((post, index) => {
-   return <Post _id={post._id} title={post.title} imgURL={post.imgURL} key={index} />
+    return <Post _id={post._id} title={post.title} imgURL={post.imgURL} textSample={post.content} key={index} />
   })
   
-  
   return (
-    <>
+    <div className="posts-container">
       {/* <Search onSubmit={handleSubmit} onChange={handleSearch} />
       <Sort onSubmit={handleSubmit} onChange={handleSort} /> */}
       <div className="posts">
         {postsJSX}
-      </div>
-      
-    </>
+      </div> 
+    </div>
   )         
 }
 

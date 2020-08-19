47aab7f4a8774c1b754b4810b85d75df1ec6c548
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-19 00:31:10 -0400

    going to try some sass

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -5,16 +5,17 @@ import { Link } from 'react-router-dom'
 function Post(props) {
   
   return (
-    <>
-      <Link className="post" to={`/post/${props._id}`}>
+    <div className="story-wrapper">
+      <Link className="post" to={`/posts/${props._id}`}>
+        <div className="img-title-container">
         <img src={props.imgURL}
           className="post-image" />
-        <div className="post-title">{props.title}</div>
+          <div className="post-title">{props.title}</div>
+          </div>
         <div className="author">placeholder author name</div>
-        <div className="summary-text">{props.textSample.substring(0, 100)}</div>
+        <div className="summary-text">{`${props.textSample.substring(0, 100)}${props.textSample.substring(98, 100).includes('.') ? '' : '...'}`}</div>
       </Link>
-
-    </>
+    </div>
   )
 }
 

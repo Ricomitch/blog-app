69597d336e52485fa27744e06207f1a3cd41b3de
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-19 19:14:04 -0400

    filter function

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -3,20 +3,23 @@ import './Post.css'
 import { Link } from 'react-router-dom'
 
 function Post(props) {
-  
+
   return (
-  
+    <div className="post">
       <Link className="post" to={`/posts/${props._id}`}>
         <div className="img-title-container">
           <img src={props.imgURL}
-          className="post-image" />
+            className="post-image" />
           <div className="post-title">{props.title}</div>
         </div>
-        <div className="author">{props.authorName}</div>
+      </Link>
+      <div className="author" onClick={(e) => props.handleFilter(e)}>{props.authorName}</div>
+      <Link className="post" to={`/posts/${props._id}`}>
         <div className="summary-text">{`${props.textSample.substring(0, 100)}${props.textSample.substring(98, 100).includes('.') ? '' : '...'}`}</div>
       </Link>
+    </div>
   )
-  
+
 }
 
 export default Post

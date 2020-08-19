2c520c501876f9ee5263ba3e76a8fbc53493ecf6
SOrmiston <sean.david.ormiston@gmail.com>
2020-08-19 11:54:50 -0400

    refactored css + bylines added to post cards

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -11,9 +11,11 @@ function Post(props) {
           <img src={props.imgURL}
           className="post-image" />
           <div className="post-title">{props.title}</div>
-          </div>
-        <div className="author">placeholder author name</div>
-        <div className="summary-text">{`${props.textSample.substring(0, 100)}${props.textSample.substring(98, 100).includes('.') ? '' : '...'}`}</div>
+      </div>
+      <div className="sub-title">
+          <div className="by-line">placeholder author name</div>
+          <div className="summary-text">{`${props.textSample.substring(0, 100)}${props.textSample.substring(98, 100).includes('.') ? '' : '...'}`}</div>
+      </div>
       </Link>
     
   )
